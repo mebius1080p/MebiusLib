a9229e45cda13417ef899424fdca97e2cee46f2f
mebius <mebius1080p@gmail.com>
2015-01-17 23:21:28 +0900

    Add LogSender, LogEventArgs

---
+++ LogEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MebiusLib
+{
+    class LogEventArgs : EventArgs
+    {
+        public string Log { get; private set; }
+        public LogEventArgs(string aLog)
+        {
+            this.Log = aLog;
+        }
+    }
+}
---
+++ LogSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using プロジェクトの名前空間を追加
+
+namespace MebiusLib
+{
+    static class LogSender
+    {
+        public static event Action<LogEventArgs> LogEvent;
+        public static void SendLog(string aLog)
+        {
+            App.Current.Dispatcher.Invoke(LogSender.LogEvent, new LogEventArgs(aLog));
+        }
+    }
+}
