719345842b77c0209d5da43618aac61ad883d388
mebius <mebius1080p@gmail.com>
2015-01-17 23:14:29 +0900

    Add AutoScrollBehavior

---
+++ AutoScrollBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MebiusLib
+{
+    class AutoScrollBehavior : Behavior<ListBox>
+    {
+        private ObservableCollection<string> oc;
+        public AutoScrollBehavior() { }//do nothing
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.oc = (ObservableCollection<string>)this.AssociatedObject.ItemsSource;
+            oc.CollectionChanged += this.scroll;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.oc.CollectionChanged -= this.scroll;
+        }
+        private void scroll(object sender, EventArgs e)
+        {
+            this.AssociatedObject.ScrollIntoView(this.oc[this.oc.Count - 1]);
+        }
+    }
+}
